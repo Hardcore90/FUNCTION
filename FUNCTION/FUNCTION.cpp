bd36b8dc7b465163ab78f2e7f0a47e8934caa92f
Dmitriy <maradonna05@yandex.ru>
2023-02-12 12:16:56 +0300

    Добавьте файлы проекта.

---
+++ FUNCTION/FUNCTION.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+void printArray(int array[], int size)
+{
+    srand(time(NULL));
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = 1 + rand() % 19;
+        printf("%3d" , array[i]);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    printf("Задание\n\nОбъявить два целочисленных массива с разными размерами и написать функцию, которая заполняет их элементы значениями и выводит на экран.\n\nФункция должна принимать два параметра – массив и его размер.\n\n");
+
+    int arr1[10];
+    int arr2[8];
+    int s1 = 10;
+    int s2 = 8;
+
+    printArray(arr1, s1);
+    printf("\n\n");
+    printArray(arr2, s2);
+}
